82b19db710f4ae75449abdf20bfb17d5c4fc32de
Luis <luis.prax@icloud.com>
2022-11-12 21:06:45 +0100

    Button

--- 02_Homework/sketch.js
+++ 02_Homework/sketch.js
@@ -1,6 +1,9 @@
 let RectWidth = 50;
 let DataRadius = 300;
+let rotateAnimation = 0;
 let slider;
+let OneStep;
+let button;
 
 
 function preload() {
@@ -8,6 +11,9 @@ function preload() {
 }
 
 function setup() {
+
+    OneStep = (360 / table.getRowCount());
+
     createCanvas(windowWidth, windowHeight);
     background(245);
     noStroke();
@@ -19,32 +25,40 @@ function setup() {
     print(table.getColumnCount() + ' total columns in table');
     print('All cities:', table.getColumn('current_city'));
 
+   /* slider = createSlider(0, 360, 360, OneStep);
+    slider.position (10,10);
+    slider.style('width', '200px');
+    slider.style('margin', '0 auto');*/
 
-    slider = createSlider(0, 360, 100);
-    slider.position(10, 10);
+    button = createButton('click me');
+    button.position(40, 40);
+    button.mouseClicked(Click);
+
 }
 
 function draw() {
 
-    let val = slider.value();
-
     background(255);
+    //let val = slider.value();
+
     push();
     translate(width / 2, height / 1);
-    rotate(val);
+    rotate(rotateAnimation);
+    //rotate(val);
     Data();
-    fill('grey');
-    circle(0, 0, DataRadius*2+10);
+    strokeWeight(8);
+    stroke(0);
+    circle(0, 0, DataRadius * 2 + 10);
     pop();
 
-    push();
-    textAlign(CENTER);
-    translate(200,200);
-    rotate(90);
-    fill('black');
-    textSize(100)
-    text('black', 0, 0,);
-    pop();
+}
+
+function Click() {
+    if (rotateAnimation <= 360-OneStep){
+    rotateAnimation += 360 / table.getRowCount();
+    }else if (rotateAnimation >= 360-OneStep) {
+        rotateAnimation = 0;
+    }
 }
 
 function convertDegreesToPosition(temp) {
@@ -56,39 +70,60 @@ function convertDegreesToPosition(temp) {
 }
 
 function Data() {
+    rotate(8);
     for (let i = 0; i < table.getRowCount(); i++) {
         const city = table.get(i, 'current_city');
         const meanTemp = round(table.get(i, 'Annual_Mean_Temperature'), 1);
         const futureMeanTemp = round(table.get(i, 'future_Annual_Mean_Temperature'), 1);
+        const rotationData = i * (OneStep);
 
         push();
+        rotate(rotationData);
         const futureYPosition = convertDegreesToPosition(futureMeanTemp);
         const futureXPosition = DataRadius;
         fill('red');
-        rotate(i * (360/table.getRowCount()));
-        rect(futureXPosition, -RectWidth, futureYPosition, RectWidth, 0, 10, 10, 0);
+        rect(futureXPosition, 0, futureYPosition, RectWidth, 0, 10, 10, 0);
 
         const YPosition = convertDegreesToPosition(meanTemp);
         const XPosition = DataRadius;
         fill('blue');
-        rect(XPosition, 0, YPosition, RectWidth, 0, 10, 10, 0);
+        rect(XPosition, -RectWidth, YPosition, RectWidth, 0, 10, 10, 0);
         pop();
 
+        //////////////////////////////////////////////////////////////
+
+
+        //TEMPERATURE TEXT;
+
+        textAlign(CENTER);
+
+        push();
+        rotate(rotationData);
+        translate(YPosition + 10, -RectWidth / 2);
+        const meanTemplabel = `${meanTemp}°C`;
+        const futureTemplabel = `${futureMeanTemp}°C`;
+        fill('blue');
+        rotate(90);
+        text(meanTemplabel, 0, -DataRadius);
+        pop();
+
+        push();
+        rotate(rotationData);
+        translate(futureYPosition + 10, -RectWidth / 2);
+        fill('red');
+        rotate(90);
+        text(futureTemplabel, RectWidth, -DataRadius);
+        pop();
+
+        push();
+        rotate(rotationData);
+        translate(720, 0);
         const label = `${city}`;
         fill('black');
-        push();
-        rotate(i * (360/table.getRowCount()));
-        translate(600,0);
         rotate(90);
-        text(label, 0, 0);
+        textSize(80);
+        text(label, 0, 100);
         pop();
 
-        const meanTemplabel = `${meanTemp}°C`;
-        fill('blue');
-        text(meanTemplabel, 40 + XPosition, 460);
-
-        const futureTemplabel = `${futureMeanTemp}°C`;
-        fill('red');
-        text(futureTemplabel, 40 + XPosition, 480);
     }
 }
